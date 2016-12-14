de168c91ac594714d099145721ca2be6a09749dc
carles san agustin <carles.sam@gmail.com>
2016-12-14 12:54:41 +0100

    exit on any transfer process failure

--- cp_s3_to_s3.py
+++ cp_s3_to_s3.py
@@ -74,11 +74,11 @@ for line in lines:
     destination_file_path = line.split('/')[5]
 
     if disk_usage('/') >= max_pct:
-        my_logger.error('Disk space failure')
+        my_logger.error('Disk space limit failure.')
         exit(0)
 
+    print 'Processing: '+source_file_path+'/'+source_file_name
     try:
-        print 'Processing: '+source_file_path+'/'+source_file_name
         conn_orig.download_file(orig_bucket_name,
                                 source_file_path+'/'+source_file_name,
                                 source_file_name)
@@ -89,6 +89,7 @@ for line in lines:
     except Exception, e:
         my_logger.info('FAIL: '+source_file+' :: '+source_file_path+'/'+source_file_name)
         my_logger.error('Dump message: '+ str(e))
+        exit(0)
 
 total_time = 'Process time: ' + str(timedelta(seconds=time.time() - start_time))
 my_logger.info(str(args)+' :total_time: '+total_time)
